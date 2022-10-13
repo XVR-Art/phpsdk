a1fbb057dbdaa5d8ec68c8dd9f5294b586efb4fd
Jimmy from MacBook Pro <easelify@gmail.com>
2022-10-13 13:21:02 +0800

    新增头像列表接口

---
+++ src/request/AvatarListRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @copyright Copyright (c), 2010-2022, Haikou Taozhi Network Technology Co., Ltd.
+ * @author Super Junior <easelify@gmail.com>
+ * @link https://github.com/XVR-Art/phpsdk
+ */
+
+declare(strict_types=1);
+
+namespace xvrart\request;
+
+class AvatarListRequest extends \xvrart\BaseRequest
+{
+    public string $api_name = 'avatar/list';
+
+    public int $page = 1;
+
+    public int $page_size = 20;
+
+    public function getData(): array
+    {
+        $data = [];
+        $this->page !== null && $data['page'] = $this->page;
+        $this->page_size !== null && $data['page_size'] = $this->page_size;
+        return $data;
+    }
+}
